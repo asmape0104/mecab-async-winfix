02e67c323cce7d7db9ad78631ca72ac345a84ebc
asmape0104 <6086632642@g.ecc.u-tokyo.ac.jp>
2019-04-07 13:00:07 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const MeCab = require('mecab-async');
+
+if (process.platform==='win32') {
+    MeCab.prototype._shellCommand = function(str) {
+        return 'echo ' + str.replace(/&/g, '^&') + ' | ' + this.command;
+    }
+}
+
+module.exports = MeCab;
