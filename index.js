e6545b3c3c2574681a03a1df862a70b898a0f9f1
asmape0104 <6086632642@g.ecc.u-tokyo.ac.jp>
2019-04-07 15:22:10 +0900

    修正を入れる

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const MeCab = require('mecab-async');
 
 if (process.platform==='win32') {
     MeCab.prototype._shellCommand = function(str) {
-        return 'echo ' + str.replace(/&/g, '^&') + ' | ' + this.command;
+        return 'echo ' + str.replace(/&/g, '^^^&') + ' | ' + this.command;
     }
 }
 
